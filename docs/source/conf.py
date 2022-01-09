b1ec97d57af44ec76fb92ba45231cf6cdf0de00d
YCH <chnyyk@gmail.com>
2022-01-09 21:47:09 +0800

    project info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,11 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PostgreSQL 教程'
+copyright = '2021, YCH'
+author = 'YCH'
 
-release = '0.1'
+release = '0.1.0'
 version = '0.1.0'
 
 # -- General configuration
